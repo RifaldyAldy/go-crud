338c03c54ef6b0f4dc07d62918bf0bf8e661b549
RifaldyAldy <112386802+RifaldyAldy@users.noreply.github.com>
2024-01-18 11:55:04 +0700

    Add files via upload

---
+++ entities/pasien.go
@@ -0,0 +1,12 @@
+package entities
+
+type pasien struct{
+	Id int64
+	NamaLengkap string
+	NIK string
+	JenisKelamin string
+	TempatLahir string
+	TanggalLahir string
+	Alamat string
+	NoHp string
+}
